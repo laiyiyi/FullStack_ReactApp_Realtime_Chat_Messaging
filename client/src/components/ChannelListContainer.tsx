8ce880391a75ce8e8dc1bebd2d2799775897d94a
Charlotte Lai <laiyiyihan0315@gmail.com>
2023-08-04 17:15:13 +0800

    Initialization

---
+++ client/src/components/ChannelListContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+
+// import {ChannelSearch, TeamChannelList, TeamChannelPreview} from './';
+
+const ChannelListContainer = () => {
+  return (
+    <div>ChannelListContainer</div>
+  )
+}
+
+export default ChannelListContainer
